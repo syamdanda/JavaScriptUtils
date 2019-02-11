0a17a8f04aabdc9e5aa98fb53044c71b190ac2a1
Syam Danda <syam2011@gmail.com>
2019-02-11 15:52:51 +0530

    Update dateTimeUtils.js

--- dateTimeUtils.js
+++ dateTimeUtils.js
@@ -11,3 +11,18 @@ Date.prototype.getWeek = function() {
     return Math.ceil((((this - dayOne) / 86400000) + dayOne.getDay() + 1) / 7);
     //return $.datepicker.iso8601Week(this); //alternative way by using datepicker
 }
+
+//Get next working day (not including Saturday and Sunday)
+function getNextWorkingDay(date) {
+	var currentDate = date;
+	var canRepeat = true;
+	while (canRepeat) {
+		var nextDay = new Date(currentDate.setDate(currentDate.getDate() + 1));
+		if (nextDay.getDay() == 6 || nextDay.getDay() == 0) {
+			currentDate.setDate(currentDate.getDate() + 1);
+			canRepeat = true;
+		} else {
+			return nextDay;
+		}
+	}	
+}
