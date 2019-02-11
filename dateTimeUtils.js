8e8022d0c52b383ad06685c95f9ebbb57ca3e600
Syam Danda <syam2011@gmail.com>
2019-02-11 15:51:06 +0530

    Update dateTimeUtils.js

--- dateTimeUtils.js
+++ dateTimeUtils.js
@@ -4,3 +4,10 @@
 function getWeekDays() {
 	return ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 }
+
+//Get week number
+Date.prototype.getWeek = function() {
+    var dayOne = new Date(this.getFullYear(), 0, 1);
+    return Math.ceil((((this - dayOne) / 86400000) + dayOne.getDay() + 1) / 7);
+    //return $.datepicker.iso8601Week(this); //alternative way by using datepicker
+}
