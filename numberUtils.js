b301a357d048dd890e8338eb4b0d597852903b17
Syam Danda <syam2011@gmail.com>
2019-06-06 12:39:26 -0400

    added isPrime

--- numberUtils.js
+++ numberUtils.js
@@ -9,3 +9,14 @@ function isOdd(num) {
 function isEven(num){
   return num % 2 == 0;
 }
+
+// return true if the number is even
+function isPrime(num){
+  if(num==2) 
+    return true;
+  for(i=2;i<Math.sqrt(num);i++) {
+    if(num % i==0) 
+      return false; // otherwise it's a prime no.
+  }
+  return true;
+}
