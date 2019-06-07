42bfaea3e2785c611f9d0e588107eca917aa7195
Syam Danda <syam2011@gmail.com>
2019-06-07 23:13:31 +0530

    Update dateTimeUtils.js

--- dateTimeUtils.js
+++ dateTimeUtils.js
@@ -140,3 +140,34 @@ function timeDifference(current, previous) {
          return Math.round(elapsed/msPerYear ) + ' years ago';   
     }
 }
+
+function isDate(txtDate) {
+    var currVal = txtDate;
+    if(currVal == '')
+        return false;
+    
+    var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/; //Declare Regex
+    var dtArray = rxDatePattern.test(txtDate); // is format OK?
+    
+    if (! dtArray) 
+        return false;
+    
+    //Checks for mm/dd/yyyy format.
+    dtMonth = dtArray[1];
+    dtDay= dtArray[3];
+    dtYear = dtArray[5];        
+    
+    if (dtMonth < 1 || dtMonth > 12) 
+        return false;
+    else if (dtDay < 1 || dtDay> 31) 
+        return false;
+    else if ((dtMonth==4 || dtMonth==6 || dtMonth==9 || dtMonth==11) && dtDay ==31) 
+        return false;
+    else if (dtMonth == 2) 
+    {
+        var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+        if (dtDay> 29 || (dtDay ==29 && !isleap)) 
+                return false;
+    }
+    return true;
+}
